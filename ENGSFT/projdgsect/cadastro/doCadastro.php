087f482cafb74b3da162e0c18637d5f09f1bfaf9
DAVIG <davigabriel.ggwp@gmail.com>
2026-03-21 15:28:33 +0000

    update

---
+++ ENGSFT/projdgsect/cadastro/doCadastro.php
@@ -0,0 +1,20 @@
+<?php
+    //Recebendo os valores do form
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $area = $_POST['area'];
+    $preco = $_POST['preco'];
+    $moeda = $_POST['moeda'];
+    $pais = $_POST['pais'];
+
+        //Validando os campos
+        if($nome =="")
+        {
+        echo "Digite algo";
+        }
+        else
+        {
+            //testando
+            echo porra toda com br
+        }
+?>
